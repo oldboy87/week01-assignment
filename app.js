bdae980af98c56b59e588a29aa623121bc0a1972
oldboy87 <tomstevenson87@gmail.com>
2025-11-23 16:38:44 +0000

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Hello world!");
+
+let scrollCount = 0;
+let idSection = "#about";
+
+const idArray = ["#about", "#automotive", "#maritime", "#aviation", "#space", "#home", "./404"];
+
+function myFunction(){
+        console.log(idSection);
+        window.location.href = idSection;
+        idSection = idArray[scrollCount];
+        scrollCount = (scrollCount + 1);
+
+}
+
+
+
+clickBtn.addEventListener("click", function() {
+    setInterval(myFunction,100);
+})
